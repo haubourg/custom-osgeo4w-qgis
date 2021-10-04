524617e3fc03f5ab6fe2f8f38675ecf6fa46d417
Nicolas <nicolas.godet@outlook.fr>
2021-10-04 20:05:06 +0200

    Avoid the creation of default QGIS Desktop shortcut

--- qgis-custom/etc/postinstall/qgis-custom.bat
+++ qgis-custom/etc/postinstall/qgis-custom.bat
@@ -7,6 +7,11 @@ set OSGEO4W_ROOT=%OSGEO4W_ROOT:\=\\%
 
 set APPNAME=QGIS CUSTOM (LTR)
 
+@REM Modify QGIS_WIN_APP_NAME and re-do --postinstall.
+@REM Avoid the creation of default QGIS Desktop shortcut (https://trac.osgeo.org/osgeo4w/ticket/694)
+for %%i in ("%OSGEO4W_STARTMENU%") do set QGIS_WIN_APP_NAME=%%~ni\%APPNAME%
+call "%OSGEO4W_ROOT%\bin\qgis-ltr.bat" --postinstall
+
 @REM  backup native QGIS bat files and shortcuts
 
 move /Y "%OSGEO4W_ROOT%\bin\qgis-ltr.bat" "%OSGEO4W_ROOT%\bin\qgis-ltr-natif.bat"
